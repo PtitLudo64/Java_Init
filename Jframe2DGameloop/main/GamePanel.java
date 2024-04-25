b40c26a6e8c5830cb8517a68181f78b33521e148
PtitLudo64 <ludovic.p.777@gmail.com>
2024-04-25 16:33:42 +0200

    Add world map and camera movements

--- Jframe2DGameloop/main/GamePanel.java
+++ Jframe2DGameloop/main/GamePanel.java
@@ -21,13 +21,19 @@ public class GamePanel extends JPanel implements Runnable{
     public final int screenWidth = tileSize * maxScreenCol;    // 16*3*16 = 768px
     public final int screenHeight = tileSize * maxScreenRow;   // 16*3*12 = 576px
 
+    // World settings
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
     int FPS = 30;
     TileManager tileM = new TileManager(this);
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
-    Player player = new Player(this, keyH);
+    public Player player = new Player(this, keyH);
 
     // Set player's default position
     // int playerX = 100;
