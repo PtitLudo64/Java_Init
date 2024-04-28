c2266b0e4e673380355ef3a555dffd63ce10ca67
PtitLudo64 <ludovic.p.777@gmail.com>
2024-04-28 18:00:20 +0200

    Music and SoundEffects

--- Jframe2DGameloop/main/GamePanel.java
+++ Jframe2DGameloop/main/GamePanel.java
@@ -25,18 +25,22 @@ public class GamePanel extends JPanel implements Runnable{
     // World settings
     public final int maxWorldCol = 50;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
+    // public final int worldWidth = tileSize * maxWorldCol;
+    // public final int worldHeight = tileSize * maxWorldRow;
 
-    int FPS = 30;
+    int FPS = 60;
+
+    // System
     TileManager tileM = new TileManager(this);
-
     KeyHandler keyH = new KeyHandler();
-    Thread gameThread;
+    Sound sound = new Sound();
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    Thread gameThread;
+
+    // Entity and Objects
     public Player player = new Player(this, keyH);
     public SuperObject obj[] = new SuperObject[10];
-    public AssetSetter aSetter = new AssetSetter(this);
 
     // Set player's default position
     // int playerX = 100;
@@ -62,6 +66,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     public void setupGame() {
         aSetter.setObject();
+        playMusic(0);
     }
 
     public void startGameThread() {
@@ -175,5 +180,17 @@ public class GamePanel extends JPanel implements Runnable{
         // Player
         player.draw(g2);
         g2.dispose();
+    }
+    public void playMusic(int i) {
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+    public void stopMusic() {
+        sound.stop();
+    }
+    public void playSE(int i) {
+        sound.setFile(i);
+        sound.play();
     }
 }
